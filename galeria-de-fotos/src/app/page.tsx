f8fe988b6719fb3910ba4dcd2d4931a2ca59c717
Leonardo Zianni <leonardoziannii@gmail.com>
2024-05-21 11:11:23 -0300

    Feita a lógica para o sistema de galeria de fotos funcionar

--- galeria-de-fotos/src/app/page.tsx
+++ galeria-de-fotos/src/app/page.tsx
@@ -1,19 +1,44 @@
 "use client"
 
+import { useState } from "react"
+import { Modal } from "./components/Modal"
+import { PhotoItem } from "./components/PhotoItem"
 import { photoList } from "./data/photoList"
 
 const Page = () => {
 
+    const [showModal, setShowModal] = useState(false)
+    const [imageOfModal, setImageOfModal] = useState('')
 
+    const openModal = (id: number) => {
+        const photo = photoList.find(item => item.id === id)
+        if (photo) {
+            setImageOfModal(photo.url)
+            setShowModal(true)
+        }
+    }
+
+    const closeModal = () => {
+        setShowModal(false)
+    }
 
     return (
         <div className="mx-2">
             <h1 className="text-center text-3xl font-bold my-10">Fotos Intergalacticas</h1>
             <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {photoList.map(item => (
-                    <div>...</div>
+                    <div>
+                        <PhotoItem
+                            key={item.id}
+                            photo={item}
+                            onClick={() => openModal(item.id)}
+                        />
+                    </div>
                 ))}
             </section>
+            {showModal &&
+                <Modal image={imageOfModal} closeModal={closeModal} />
+            }
         </div>
     )
 }
